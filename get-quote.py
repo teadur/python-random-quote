7c6e70dd5a0814ab4b1f6bfcf96ba481c290dc7e
Georg Kahest <georg@kahest.ee>
2021-12-15 01:18:24 +0200

    get_quotes

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,21 @@
 def main():
-  print("Keep it logically awesome.")
+    print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
 
-  print(quotes[0])
+    print(quotes[0].rstrip(), quotes[1].rstrip())
+    get_quote()
 
-if __name__== "__main__":
-  main()
+
+def get_quote():
+    from quote import quote
+
+    search = 'Jasper Fforde'
+    result = quote(search, limit=2)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
